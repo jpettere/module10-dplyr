174c35e42725206e226e25d1c6bfc64585721527
Julia Pettere <jpettere77@gmail.com>
2017-01-24 13:50:31 -0800

    Add complete exercise 1

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -11,37 +11,42 @@ library(fueleconomy)
 
 # You should now have access to the `vehicles` data.frame
 
-
 # Select the different manufacturers (makes) of the cars in this data set.
-
+makes <- vehicles$make
 
 # Use the `unique()` function to determine how many different car manufacturers
 # are represented by the data set.
-
+length(unique(makes))
 
 # Filter the data set for vehicles manufactured in 1997
-
+vehicles.1997 <- vehicles[vehicles$year > 1997, ]
 
 # Arrange the 1997 cars by highway (`hwy`) gas milage
 # Hint: use the `order()` function similar to how you would use the `max()` function.
 # See also: https://www.r-bloggers.com/r-sorting-a-data-frame-by-the-contents-of-a-column/
-
+vehicles.1997 <- vehicles.1997[order(vehicles.1997$hwy), ]
 
 # Mutate the 1997 cars data frame to add a column `average` that has the average gas milage between
 # city and highway for each car
+vehicles.1997$average <- (vehicles.1997$hwy + vehicles.1997$cty) / 2
+View(vehicles.1997)
 
 
 # Filter the whole vehicles data set for 2-Wheel Drive vehicles that get more than 20 miles/gallon in the city
 # Save this new data frame in a variable.
-
+vehicles.20 <- vehicles[vehicles$drive == '2-Wheel Drive' & vehicles$cty > 20,]
 
 # Of those vehicles, what is the vehicle ID of the vehicle with the worst hwy mpg?
 # Hint: filter for the worst vehicle, then select its ID.
-
+vehicle.worst <- vehicles.20$id[vehicles.20$hwy == min(vehicles.20$hwy)]
 
 # Write a function that takes a `year` and a `make` as parameters,
 # and returns the vehicle that gets the most hwy miles/gallon of vehicles of that make in that year
 # You'll need to filter more!
-
+vehicle.best <- function(year, make) {
+  filtered.vehicles <- vehicles[vehicles$make == make & vehicles$year == year,]
+  return(filtered.vehicles[filtered.vehicles$hwy == max(filtered.vehicles$hwy),])
+}
 
 # What was the most efficient honda model of 1995?
+vehicle.best(1995, "Honda")
